9e146ed78907aaaab6477f89b5454fc65ee22f28
Vedblade <18503283+yad439@users.noreply.github.com>
2021-05-07 22:09:54 +0700

    Fix getDownloadInfo function

--- lib/data/yandex_music_datasource.dart
+++ lib/data/yandex_music_datasource.dart
@@ -6,12 +6,14 @@ import 'json/download_info.dart';
 
 class YandexMusicDatasource {
   Future<DownloadInfo> getDownloadInfo(int trackId) async {
-    final infoUrlJsonResponse = await http.get(Uri.https('music.yandex.ru',
-        'api/v2.1/handlers/track/$trackId/track/download/m?hq=1'));
+    final infoUrlJsonResponse = await http.get(
+        Uri.https('music.yandex.ru',
+            'api/v2.1/handlers/track/$trackId/track/download/m', {'hq': '1'}),
+        headers: {'X-Retpath-Y': 'https%3A%2F%2Fmusic.yandex.ru%2F'});
     final infoUrlJson = jsonDecode(infoUrlJsonResponse.body);
 
-    final infoJson =
-        await http.get(Uri.parse(infoUrlJson["src"] + '&format=json'));
+    final infoJson = await http
+        .get(Uri.parse('https:' + infoUrlJson["src"] + '&format=json'));
     return DownloadInfo.fromJson(jsonDecode(infoJson.body));
   }
 }
