fbb32824c995b213de32e2a92b8e86fbfbc48a53
Vedblade <18503283+yad439@users.noreply.github.com>
2021-09-05 19:13:05 +0700

    Add player events

---
+++ lib/presentation/bloc/player_event.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/entity/track.dart';
+
+part 'player_event.freezed.dart';
+
+@immutable
+@freezed
+class PlayerEvent with _$PlayerEvent {
+  const factory PlayerEvent.pause() = _Pause;
+
+  const factory PlayerEvent.resume() = _Resume;
+
+  const factory PlayerEvent.stop() = _Stop;
+
+  const factory PlayerEvent.play(Track track) = _Play;
+}
