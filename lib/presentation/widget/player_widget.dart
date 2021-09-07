297d6ba862032da7f538ec8627e81bd4e31b8322
Vedblade <18503283+yad439@users.noreply.github.com>
2021-09-07 20:09:03 +0700

    Add player widget

---
+++ lib/presentation/widget/player_widget.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+
+import '../../domain/entity/artist.dart';
+import '../../domain/entity/album.dart';
+import '../../domain/entity/track.dart';
+import '../bloc/player_event.dart';
+import '../bloc/player_bloc.dart';
+
+class PlayerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.read<PlayerBloc>();
+    return Row(
+      children: [
+        StreamBuilder<bool>(
+            stream: bloc.isPlaying,
+            initialData: false,
+            builder: (cont, playing) => playing.data!
+                ? TextButton(
+                    child: Text("||"),
+                    onPressed: () => bloc.command.add(PlayerEvent.pause()))
+                : TextButton(
+                    child: Text("|>"),
+                    onPressed: () => bloc.command.add(PlayerEvent.play(Track(
+                        5493020,
+                        "test",
+                        AlbumMin(0, "qwer"),
+                        [ArtistMin(0, "asdf")]))),
+                  )),
+        Expanded(
+          child: Column(
+            children: [
+              StreamBuilder<Track?>(
+                stream: bloc.currentTrack,
+                initialData: null,
+                builder: (cont, snap) {
+                  final track = snap.data;
+                  if ((track == null)) {
+                    return Text('-');
+                  } else {
+                    var artist = track.artists.map((ar) => ar.name).join(';');
+                    return Text('$artist - ${track.title}');
+                  }
+                },
+              ),
+              SizedBox(
+                height: 20,
+                child: StreamBuilder<double>(
+                  stream: bloc.progress,
+                  initialData: 0.0,
+                  builder: (cont, progress) => LinearProgressIndicator(
+                    value: progress.data!,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  StreamBuilder<Duration>(
+                    stream: bloc.position,
+                    initialData: const Duration(),
+                    builder: (cont, snap) => Text(snap.data!.toString()),
+                  ),
+                  Text('/'),
+                  StreamBuilder<Duration>(
+                    stream: bloc.duration,
+                    initialData: const Duration(),
+                    builder: (cont, snap) => Text(snap.data!.toString()),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
