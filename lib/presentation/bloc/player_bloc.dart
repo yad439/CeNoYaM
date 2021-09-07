2723e2c881b23bc3da38ea1c62ce854b889ed15b
Vedblade <18503283+yad439@users.noreply.github.com>
2021-09-07 20:08:34 +0700

    Add player bloc

---
+++ lib/presentation/bloc/player_bloc.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+
+import '../../domain/entity/track.dart';
+import '../../domain/yandex_player.dart';
+import 'player_event.dart';
+
+class PlayerBloc {
+  final YandexPlayer _player;
+  final StreamController<Track?> _trackController = StreamController<Track?>();
+  final StreamController<bool> _playingController = StreamController<bool>();
+  final StreamController<PlayerEvent> _eventController =
+  StreamController<PlayerEvent>();
+
+  PlayerBloc(this._player) {
+    _eventController.stream.listen(_handleEvent);
+  }
+
+  Sink<PlayerEvent> get command => _eventController.sink;
+
+  Stream<bool> get isPlaying => _playingController.stream;
+
+  Stream<Duration> get duration => _player.durationStream;
+
+  Stream<Duration> get position => _player.position;
+
+  Stream<double> get progress =>
+      _player.position.asyncMap((event) =>
+          _player.duration.then((value) => value.toDouble() / event.inSeconds));
+
+  Stream<Track?> get currentTrack => _trackController.stream;
+
+  void _handleEvent(PlayerEvent event) =>
+      event.when(
+          pause: () => _pause(),
+          resume: () => _resume(),
+          stop: () => _stop(),
+          play: (track) => _play(track));
+
+  void _pause() {
+    _player.pause();
+    _playingController.sink.add(false);
+  }
+
+  void _resume() {
+    _player.resume();
+    _playingController.sink.add(true);
+  }
+
+  void _stop() {
+    _player.stop();
+    _playingController.sink.add(false);
+    _trackController.sink.add(null);
+  }
+
+  void _play(Track track) {
+    _player.play(track.id);
+    _trackController.sink.add(track);
+    _playingController.sink.add(true);
+  }
+
+  void dispose() {
+    _eventController.close();
+    _trackController.close();
+    _playingController.close();
+  }
+}
