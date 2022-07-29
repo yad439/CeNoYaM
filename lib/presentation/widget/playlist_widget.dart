3bb9a475506e644c7bb78c8a8a97d5361f0d1881
Cirrus CI <support@cirruslabs.org>
2022-07-29 06:28:00 +0000

    Fix playlist widget

--- lib/presentation/widget/playlist_widget.dart
+++ lib/presentation/widget/playlist_widget.dart
@@ -1,8 +1,8 @@
-import 'package:cenoyam/presentation/bloc/playlist_state.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/playlist_bloc.dart';
+import '../bloc/playlist_state.dart';
 import 'track_widget.dart';
 
 class PlaylistWidget extends StatelessWidget {
@@ -11,10 +11,12 @@ class PlaylistWidget extends StatelessWidget {
     return BlocBuilder<PlaylistBloc, PlaylistState>(
         builder: (context, state) => state.when(
             uninitialized: () => const SizedBox(),
-            loaded: (tracks) => ListView(
-                  children: tracks
-                      .map((track) => TrackWidget(track, () {}))
-                      .toList(growable: false),
+            loaded: (tracks) => Expanded(
+                  child: ListView(
+                    children: tracks
+                        .map((track) => TrackWidget(track, () {}))
+                        .toList(growable: false),
+                  ),
                 )));
   }
 }
