0a82830f92c4d64d5079651dccd9960ae5399956
Vedblade <18503283+yad439@users.noreply.github.com>
2022-08-01 15:55:07 +0000

    Add search structures

---
+++ lib/domain/entity/search_results.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'album.dart';
+import 'artist.dart';
+import 'playlist.dart';
+import 'track.dart';
+
+part 'search_results.freezed.dart';
+
+@immutable
+@freezed
+class SearchResults with _$SearchResults {
+  const factory SearchResults(
+    List<TrackMin> tracks,
+    List<ArtistMin> artists,
+    List<AlbumMin> albums,
+    List<Playlist> playlists,
+  ) = _SearchResults;
+}
---
+++ lib/domain/search_type.dart
@@ -0,0 +1,1 @@
+enum SearchType { all, tracks, artists, albums, playlists }
