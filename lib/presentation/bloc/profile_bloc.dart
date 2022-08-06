b6ee439e7148761874f7ca45d60486c3d9f47378
Vedblade <18503283+yad439@users.noreply.github.com>
2022-08-06 07:38:31 +0000

    Add profile bloc

---
+++ lib/presentation/bloc/profile_bloc.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../domain/music_repository.dart';
+import 'profile_event.dart';
+import 'profile_state.dart';
+
+class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
+  ProfileBloc(this._repository) : super(const ProfileState.unknown()) {
+    on<ProfileEvent>((event, emit) {
+      assert(event == ProfileEvent.update, 'Unexpected event');
+      return _update(emit);
+    });
+  }
+  final MusicRepository _repository;
+
+  Future<void> _update(Emitter<ProfileState> emit) async {
+    final username = await _repository.getUsername();
+    if (username == null) {
+      emit(const ProfileState.anonimous());
+    } else {
+      emit(ProfileState.loggedIn(username));
+    }
+  }
+}
