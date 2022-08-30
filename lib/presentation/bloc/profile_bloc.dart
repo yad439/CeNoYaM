ca6fcfd617532224e19054c05c36f9d967cd8f41
Vedblade <18503283+yad439@users.noreply.github.com>
2022-08-30 10:57:32 +0000

    Make profile bloc drop concurrent requests

--- lib/presentation/bloc/profile_bloc.dart
+++ lib/presentation/bloc/profile_bloc.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../domain/music_repository.dart';
@@ -6,10 +7,13 @@ import 'profile_state.dart';
 
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
   ProfileBloc(this._repository) : super(const ProfileState.unknown()) {
-    on<ProfileEvent>((event, emit) {
-      assert(event == ProfileEvent.update, 'Unexpected event');
-      return _update(emit);
-    });
+    on<ProfileEvent>(
+      (event, emit) {
+        assert(event == ProfileEvent.update, 'Unexpected event');
+        return _update(emit);
+      },
+      transformer: droppable(),
+    );
   }
   final MusicRepository _repository;
 
